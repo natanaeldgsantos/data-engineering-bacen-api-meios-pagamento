495659e1d3b567f79a224c92c83b4b6d637affc6
Natanael Domingos <natanaeldgsantos@gmail.com>
2025-01-05 11:52:52 -0300

    add docker-compose lets start working with minioIO has Datalake Storage

---
+++ src/params.py
@@ -0,0 +1,11 @@
+""" Parâmetros Globais e variáveis de Ambiente do Projeto """
+
+
+import os
+
+
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(PROJECT_DIR, 'data')
+
+
